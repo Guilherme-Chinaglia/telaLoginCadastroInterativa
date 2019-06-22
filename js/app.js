e8c99910b8a4fd6dc4697291ca57caead84bb2ff
Guilherme Chinaglia <quinalia.guilherme@gmail.com>
2019-06-22 18:22:28 -0300

    Tela de Login e Cadastro Interativa

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+var btnSignin = document.querySelector("#signin");
+var btnSignup = document.querySelector("#signup");
+
+var body = document.querySelector("body");
+
+
+btnSignin.addEventListener("click", function () {
+   body.className = "sign-in-js"; 
+});
+
+btnSignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+})
+
+
